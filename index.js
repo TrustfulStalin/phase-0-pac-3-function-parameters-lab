871811bb0222a60c6335a2e4502afa9a4425ffbb
Tevon Talbert <TevonTalbert@hotmail.com>
2023-08-18 18:12:38 -0500

    adding finished lab

---
+++ index.js
@@ -0,0 +1,21 @@
+function introduction(name) {
+  const Name = name;
+  return (`Hi, my name is ${Name}.`);
+  
+ 
+}
+
+function introductionWithLanguage(name, lanuage) {
+
+  return (`Hi, my name is ${name} and I am learning to program in ${lanuage}.`);
+
+}
+
+
+function introductionWithLanguageOptional(name , lanuage = "JavaScript") {
+
+ 
+
+  return (`Hi, my name is ${name} and I am learning to program in ${lanuage}.`);
+
+}
